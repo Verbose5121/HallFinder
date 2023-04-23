ce57006bb17e0f3715da86e437a8c7efdb9c71eb
poojamlvy <poojamlvy@gmail.com>
2023-04-23 13:14:00 -0600

    Changed scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+const style = {
+  border: "2px solid purple",
+  margin: 12,
+  padding: 8,
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [dataSource, setDataSource] = useState(Array.from({ length: 20 }));
+  const [hasMore, setHasMore] = useState(true);
+  const fetchMoreData = () => {
+    console.log('datasource', dataSource)
+    if (dataSource.length < 200) {
+      setTimeout(() => {
+        setDataSource(dataSource.concat(Array.from({ length: 20 })));
+      }, 500);
+    } else {
+      setHasMore(false);
+    }
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        {" "}
+        Title: <b>Hall Finder</b>
       </p>
+      <div id="parentScrollDiv" style={{ height: 400, overflow: "auto" }}>
+        <InfiniteScroll
+          dataLength={dataSource.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<p>Loading...</p>}
+          endMessage={<p> Done</p>}
+          // height={500}
+          scrollableTarget="parentScrollDiv"
+        >
+          {dataSource.map((item, index) => {
+            return (
+              <div style={style}>Hall number in #{index + 1} calgary area</div>
+            );
+          })}
+        </InfiniteScroll>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+

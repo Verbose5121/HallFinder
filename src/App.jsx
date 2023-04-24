62ecc364fead6e2867a75fcbb2caeea4174ed06f
MK <thisisformed@pm.me>
2023-04-23 19:39:37 -0600

    corrected autosave

--- src/App.jsx
+++ src/App.jsx
@@ -65,48 +65,5 @@ function App() {
   );
 }
 
-
-
-// const App = () => {
-//   const [dataSource, setDataSource] = useState(Array.from({ length: 20 }));
-//   const [hasMore, setHasMore] = useState(true);
-//   const fetchMoreData = () => {
-//     console.log('datasource', dataSource)
-//     if (dataSource.length < 200) {
-//       setTimeout(() => {
-//         setDataSource(dataSource.concat(Array.from({ length: 20 })));
-//       }, 500);
-//     } else {
-//       setHasMore(false);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <p>
-//         {" "}
-//         Title: <b>Hall Finder</b>
-//       </p>
-//       <div id="parentScrollDiv" style={{ height: 400, overflow: "auto" }}>
-//         <InfiniteScroll
-//           dataLength={dataSource.length}
-//           next={fetchMoreData}
-//           hasMore={hasMore}
-//           loader={<p>Loading...</p>}
-//           endMessage={<p> Done</p>}
-//           // height={500}
-//           scrollableTarget="parentScrollDiv"
-//         >
-//           {dataSource.map((item, index) => {
-//             return (
-//               <div style={style}>Hall number in #{index + 1} calgary area</div>
-//             );
-//           })}
-//         </InfiniteScroll>
-//       </div>
-//     </div>
-//   );
-// };
-
 export default App;
 

8b0c5ec74a73256a2ddcc44d9a348123c94ae256
MK <thisisformed@pm.me>
2023-04-25 12:04:21 -0600

    Added components dir for later reorg >Pooja scroll

---
+++ src/components/infiniteScroll.jsx
@@ -0,0 +1,23 @@
+      // Pooja's infinite scroll element - for later use
+      
+      /* <p>
+        {" "}
+        Title: <b>Hall Finder</b>
+      </p>
+      <div id="parentScrollDiv" style={{ height: 400, overflow: "auto" }}>
+        <InfiniteScroll
+          dataLength={dataSource.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<p>Loading...</p>}
+          endMessage={<p> Done</p>}
+          // height={500}
+          scrollableTarget="parentScrollDiv"
+        >
+          {dataSource.map((item, index) => {
+            return (
+              <div style={style}>Hall number in #{index + 1} calgary area</div>
+            );
+          })}
+        </InfiniteScroll>
+      </div> */

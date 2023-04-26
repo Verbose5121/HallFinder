e6eaa7ec7074bd6f6e3ef2fca57dda6ccfa865db
Prabh Sagoo <prabhsagoo@yahoo.com>
2023-04-25 23:50:53 -0600

    NavBar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import 'mapbox-gl/dist/mapbox-gl.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className='abc'>
-<img src="src/images/HallFinder-1 (2).png" width="150px" style={{marginTop:"0px"}} />
-<div style={{marginBottom:"50px",marginLeft:"200px" ,display:"flex", flexDirection:"column", justifyContent:"space-around", height:"100px", width:"100%", flexWrap:"wrap"}}><h1 style={{color:"#112D32", textAlign:"center", fontSize:"50px",fontFamily:"Wanderlust"}}>Welcome to HallFinder</h1>
-<h2 style={{fontFamily:"Wanderlust"}}>
-Effortless Booking Process
-</h2>
-<div><button className='btn'><a href='#data' style={{textDecoration:"none", color:"#88BDBC", fontFamily:"Wanderlust"}}>Getting Started</a></button></div></div></div>
+    <div className="abc">
+      <img
+        src="src/images/HallFinder-1 (6).png"
+        width="300px"
+        height="80px"
+        style={{ margin: "auto 40px" }}
+      />
+      <nav>
+        <ul>
+          <li>
+            <a className="nav-item" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="nav-item" href="#">
+              Events
+            </a>
+          </li>
+          <li>
+            <a className="nav-item" href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="nav-item" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

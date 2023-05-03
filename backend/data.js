63c86a53a6239ea7d37edb4265db18f405ee565d
MK <thisisformed@pm.me>
2023-05-03 09:36:48 -0600

    data.js used twice. renamed for use in backend.

--- backend/data.js
+++ backend/data.js
@@ -1,287 +1,301 @@
 export const dataGeo = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        properties: { ID: 1, isAlien: "yes",
-      name:"Marda Loop Community Association",
-    img:"src/images/halls/communityhall1.jpg",
-  address: "3130 - 16th Street SW Calgary, AB T2T 4G7, Canada" },
-        geometry: {
-          type: "Point",
-          coordinates: [-114.1011762, 51.0256322],
-        },
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        ID: 1,
+        isAlien: "yes",
+        name: "Marda Loop Community Association",
+        img: "src/images/halls/communityhall1.jpg",
+        address: "3130 - 16th Street SW Calgary, AB T2T 4G7, Canada",
       },
-      {
-        type: "Feature",
-        properties: { ID: 2, isAlien: "",name:"Strathcona Christie Aspen Community Association",
-        img:"src/images/halls/CH2.jpeg",
-      address: "277 Strathcona Dr SW, Calgary, AB T3H 2A4, Canada" },
-        geometry: {
-          type: "Point",
-          coordinates: [-114.1825231, 51.0472992],
-        },
+      geometry: {
+        type: "Point",
+        coordinates: [-114.1011762, 51.0256322],
       },
-      {
-        type: "Feature",
-        properties: { ID: 3, isAlien: "", name:"Bankview Community Association",
-        img:"src/images/halls/CH3.jpeg",
-      address: "2418 17 St SW, Calgary, AB T2T 4M8, Canada" },
-        geometry: {
-          type: "Point",
-          coordinates: [-114.1040631, 51.0325128],
-        },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 2,
+        isAlien: "",
+        name: "Strathcona Christie Aspen Community Association",
+        img: "src/images/halls/CH2.jpeg",
+        address: "277 Strathcona Dr SW, Calgary, AB T3H 2A4, Canada",
       },
-      {
-        type: "Feature",
-        properties: { ID: 4, isAlien: "", name:"Palliser Bayview Pumphill Community Association",
-        img:"src/images/halls/CH4.jpeg",
-      address: "2323 Palliser Dr SW, Calgary, AB T2V 3S4, Canada" },
-        geometry: {
-          type: "Point",
-          coordinates: [-114.1161444, 50.968834],
-        },
+      geometry: {
+        type: "Point",
+        coordinates: [-114.1825231, 51.0472992],
       },
-      {
-        type: "Feature",
-        properties: { ID: 5, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.83016967773438, 33.58602331802259],
-        },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 3,
+        isAlien: "",
+        name: "Bankview Community Association",
+        img: "src/images/halls/CH3.jpeg",
+        address: "2418 17 St SW, Calgary, AB T2T 4M8, Canada",
       },
-      {
-        type: "Feature",
-        properties: { ID: 6, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.86724853515625, 33.277731642555224],
-        },
+      geometry: {
+        type: "Point",
+        coordinates: [-114.1040631, 51.0325128],
       },
-      {
-        type: "Feature",
-        properties: { ID: 7, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.33990478515625, 33.53338195763831],
-        },
+    },
+    {
+      type: "Feature",
+      properties: {
+        ID: 4,
+        isAlien: "",
+        name: "Palliser Bayview Pumphill Community Association",
+        img: "src/images/halls/CH4.jpeg",
+        address: "2323 Palliser Dr SW, Calgary, AB T2V 3S4, Canada",
       },
-      {
-        type: "Feature",
-        properties: { ID: 8, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.23004150390625, 33.32593850874471],
-        },
+      geometry: {
+        type: "Point",
+        coordinates: [-114.1161444, 50.968834],
       },
-      {
-        type: "Feature",
-        properties: { ID: 9, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.63516235351562, 33.578014746143985],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 5, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.83016967773438, 33.58602331802259],
       },
-      {
-        type: "Feature",
-        properties: { ID: 0, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.7821044921875, 33.43717151666947],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 6, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.86724853515625, 33.277731642555224],
       },
-      {
-        type: "Feature",
-        properties: { ID: 11, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.71755981445312, 33.20652045176062],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 7, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.33990478515625, 33.53338195763831],
       },
-      {
-        type: "Feature",
-        properties: { ID: 12, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.22454833984374, 33.12950124445052],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 8, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.23004150390625, 33.32593850874471],
       },
-      {
-        type: "Feature",
-        properties: { ID: 13, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.09683227539062, 33.47841764867342],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 9, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.63516235351562, 33.578014746143985],
       },
-      {
-        type: "Feature",
-        properties: { ID: 14, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.315185546875, 33.897777013859475],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 0, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.7821044921875, 33.43717151666947],
       },
-      {
-        type: "Feature",
-        properties: { ID: 15, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.82742309570312, 33.7825716472443],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 11, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.71755981445312, 33.20652045176062],
       },
-      {
-        type: "Feature",
-        properties: { ID: 16, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-103.82354736328125, 33.710632271492095],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 12, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.22454833984374, 33.12950124445052],
       },
-      {
-        type: "Feature",
-        properties: { ID: 17, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.58297729492188, 33.19388015067254],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 13, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.09683227539062, 33.47841764867342],
       },
-      {
-        type: "Feature",
-        properties: { ID: 18, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.74639892578125, 33.073130945006625],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 14, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.315185546875, 33.897777013859475],
       },
-      {
-        type: "Feature",
-        properties: { ID: 19, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.2547607421875, 33.01326987686983],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 15, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.82742309570312, 33.7825716472443],
       },
-      {
-        type: "Feature",
-        properties: { ID: 20, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.08172607421875, 33.22375428474926],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 16, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-103.82354736328125, 33.710632271492095],
       },
-      {
-        type: "Feature",
-        properties: { ID: 21, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.98123168945311, 33.46696235807553],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 17, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.58297729492188, 33.19388015067254],
       },
-      {
-        type: "Feature",
-        properties: { ID: 22, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.45526123046875, 33.90689555128866],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 18, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.74639892578125, 33.073130945006625],
       },
-      {
-        type: "Feature",
-        properties: { ID: 23, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.28497314453125, 33.457797035354766],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 19, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.2547607421875, 33.01326987686983],
       },
-      {
-        type: "Feature",
-        properties: { ID: 24, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.52255249023438, 33.384439582098224],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 20, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.08172607421875, 33.22375428474926],
       },
-      {
-        type: "Feature",
-        properties: { ID: 25, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.61868286132812, 33.00060174664655],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 21, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.98123168945311, 33.46696235807553],
       },
-      {
-        type: "Feature",
-        properties: { ID: 26, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-105.14465332031249, 32.778037985363675],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 22, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.45526123046875, 33.90689555128866],
       },
-      {
-        type: "Feature",
-        properties: { ID: 27, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-103.81393432617188, 32.858825196463854],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 23, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.28497314453125, 33.457797035354766],
       },
-      {
-        type: "Feature",
-        properties: { ID: 28, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-105.23941040039062, 34.028762179464465],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 24, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.52255249023438, 33.384439582098224],
       },
-      {
-        type: "Feature",
-        properties: { ID: 29, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-103.68484497070312, 34.07199987534163],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 25, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.61868286132812, 33.00060174664655],
       },
-      {
-        type: "Feature",
-        properties: { ID: 30, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.12155151367188, 33.884097379274905],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 26, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-105.14465332031249, 32.778037985363675],
       },
-      {
-        type: "Feature",
-        properties: { ID: 31, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.08172607421875, 33.89207743274474],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 27, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-103.81393432617188, 32.858825196463854],
       },
-      {
-        type: "Feature",
-        properties: { ID: 32, isAlien: "yes" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.13940429687499, 33.920571528675076],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 28, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-105.23941040039062, 34.028762179464465],
       },
-      {
-        type: "Feature",
-        properties: { ID: 33, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.05975341796875, 33.93652406150093],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 29, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-103.68484497070312, 34.07199987534163],
       },
-      {
-        type: "Feature",
-        properties: { ID: 34, isAlien: "" },
-        geometry: {
-          type: "Point",
-          coordinates: [-104.10232543945312, 33.959308210392024],
-        },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 30, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.12155151367188, 33.884097379274905],
       },
-    ],
-  };
-  
+    },
+    {
+      type: "Feature",
+      properties: { ID: 31, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.08172607421875, 33.89207743274474],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 32, isAlien: "yes" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.13940429687499, 33.920571528675076],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 33, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.05975341796875, 33.93652406150093],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { ID: 34, isAlien: "" },
+      geometry: {
+        type: "Point",
+        coordinates: [-104.10232543945312, 33.959308210392024],
+      },
+    },
+  ],
+};

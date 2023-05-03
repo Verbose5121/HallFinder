38aa9ed11a4b6e069558ed08de0bb97fdb9faf8d
MK <thisisformed@pm.me>
2023-05-02 21:02:26 -0600

    Attempting to combine parts and clean css.

--- frontend/src/HallPage.jsx
+++ frontend/src/HallPage.jsx
@@ -1,9 +1,35 @@
 // placeholder for hall page
 import React from 'react'
+import EventCalendar from './EventCalendar';
+import EventCalendarBig from './components/AdminEventCalendarBig';
+// import BookingForm from './BookingForm'
+import './HallPage.css'
+// import './EventCalendar.css'
+import BookingForm from './BookingForm';
+// import MultiDatePicker from './components/MultiDatePicker';
+import { DatePicker } from '@mui/x-date-pickers';
+import DateCalendarValue from './components/DatePicker';
+import { Container } from '@mui/material';
+// import Carousel from './components/carousel';
+import Carousel2 from './components/Carousel2';
+// import { Example, Item } from './components/Carousel3.jsx';
+
+{/* <EventCalendar /> */}
 
 function HallPage() {
   return (
-    <div>HallPage</div>
+    <>
+    <Carousel2 className="carousel"/>
+    <div className='hallpage-container'>
+    
+
+    
+
+    <DateCalendarValue />
+    <BookingForm />
+
+    </div>
+    </>
   )
 }
 

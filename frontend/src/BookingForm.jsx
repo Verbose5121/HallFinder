d46cac7695ec79771298a8fa383db3c596f8a28c
MK <thisisformed@pm.me>
2023-05-02 20:50:24 -0600

    Updated with proposed fields

--- frontend/src/BookingForm.jsx
+++ frontend/src/BookingForm.jsx
@@ -1,84 +1,162 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth, logInWithEmailAndPassword, signInWithGoogle, uploadPhoto, useAuth, storage } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import "./Login.css";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { uploadPhoto, useAuth, storage } from "./firebase";
+//import './Style.css'
+//import "./style.css";
+import './HallPage.css'
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Card from '@mui/material/Card';
 
-export default function BookingForm () {
-    const currentUser = useAuth();
-    const [photo, setPhoto] = useState(null);
-    const [photoURL, setPhotoURL] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [percent, setPercent] = useState(0);
+export default function BookingForm() {
+  const currentUser = useAuth();
+  const [photo, setPhoto] = useState(null);
+  const [photoURL, setPhotoURL] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [percent, setPercent] = useState(0);
+  const [depositAmount, setDepostAmount] = useState(100);
+  function handleChange(e) {
+    if (e.target.files[0]) {
+      setPhoto(e.target.files[0])
+    }
+  }
+  // Photo id upload button 
+  function handleClick() {
+    uploadPhoto(photo, currentUser);
+  }
 
-    function handleChange(e) {
-        if (e.target.files[0]) {
-            setPhoto(e.target.files[0]) 
-        }
+  function handlePhotoUpload() {
+    if (!photo) {
+      alert("Please select an image to upload")
     }
 
-    function handleClick () {
-        uploadPhoto(photo, currentUser);
-    }
+    const storageRef = ref(storage, `/images/${currentUser.uid}/${photo.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, photo);
 
-    function handleUpload() {
-        if (!photo) 
-        {
-            alert("Please select an image to upload")
-        }
-        
-        const storageRef = ref(storage, `/images/${currentUser.uid}/${photo.name}`)
-        const uploadTask = uploadBytesResumable(storageRef, photo);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
 
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-            const percent = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            );
-            
-            // update upload progress
-            setPercent(percent);
-            },
-            (err) => console.log(err),
-            () => {
-            // retrieve download url 
-            // TODO: update current user object with imageurl
-            getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-            console.log(url);
-            });
-            }
-            );
-    }
+        // update upload progress
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+        // retrieve download url 
+        // TODO: update current user object with imageurl
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      }
+    );
+  }
 
 
-    console.log(currentUser);
-    return (
-        <div className="bookForm">
-            <form className="bookForm-form">
-            <input type="text" placeholder="Name" /> 
-            <input type="text" placeholder="####-####-####-####" />
-            <input type="file" onChange={handleChange} />
-            <button onClick={handleUpload}>
-                Upload
-            </button>
-            <p>{percent} "% done"</p>
-            <img src={photoURL} alt="Avatar" className="avatar" />
-            {console.log(photoURL)}
-            {console.log(photo)}
-            {photo && (
-            <div>
-            <img
-                className="photoid"
-                alt="not found"
-                src={photoURL}
-            />
-            <br />
-            <button onClick={() => setPhoto(null)}>Remove</button>
-            </div> )}
-            </form>
+  // console.log(currentUser);
+  return (
+      <Box
+        component="form"
+        sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch', bgcolor: '#fffffff7' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div className='bookForm-form'>
+
+          <TextField
+            id="first-name"
+            label="First Name"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          <TextField
+            id="last-name"
+            label="Last Name"
+            defaultValue=""
+            variant="filled"
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          <TextField
+            required
+            id="telephone-number"
+            label="Telephone Number"
+            defaultValue=""
+            variant="filled"
+            type='tel'
+          />
+          <TextField
+            id="last-name"
+            label="Last Name"
+            defaultValue=""
+            variant="filled"
+          />
+          <TextField
+            disabled
+            id="deposit-amount"
+            label="Required Deposit Amount"
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            }}
+            defaultValue={depositAmount}
+            variant="filled"
+          />
+          <TextField
+            id="cc-number"
+            label="Credit Card Number (for deposit)"
+            defaultValue=""
+            variant="filled"
+            minLength="16"
+            maxLength="16"
+          />
+          <TextField
+            required
+            id="image-selector"
+            label=""
+            defaultValue=""
+            type="file"
+            helperText="Select Photo-ID Image"
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            id="submitformbtn"
+            onClick={handleClick}
+          >Submit
+          </Button>
+
+          {/* <TextField 
+        required
+        id="filled-required"
+        label="Upload Picture ID"
+        defaultValue=""
+        variant="filled"
+        type='file'
+      /> */}
+
         </div>
-    )
+      </Box>
+  )
 }

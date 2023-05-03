d24a585b6ec66f9877265946ebebcc2a3144d28a
MK <thisisformed@pm.me>
2023-05-02 21:01:50 -0600

    Temporary support files for testing.

---
+++ frontend/src/components/photos/Images.jsx
@@ -0,0 +1,9 @@
+const IMAGES = {
+    image1 : new URL('./image1.jpg', import.meta.url).href,
+    image2 : new URL('./image2.jpg', import.meta.url).href,
+    image5 : new URL('./image5.jpeg', import.meta.url).href,
+    image6 : new URL('./image6.jpeg', import.meta.url).href,
+    image7 : new URL('./image7.jpg', import.meta.url).href
+}
+
+export default IMAGES
